55314c29332127bd364076cac5c1436f168d36e7
afzal-swe <afzalbhola07@gmail.com>
2023-08-07 07:50:25 +0600

    Successfully run Slider

--- resources/views/frontend/layouts/partial/slider.blade.php
+++ resources/views/frontend/layouts/partial/slider.blade.php
@@ -1,16 +1,21 @@
 <section id="slider"><!--slider-->
     <div class="container">
         <div class="row">
+            @php
+                $slider = DB::table('sliders')->where('status', '=', '1')->orderBy('id', 'DESC')->limit(4)->get();
+            @endphp
             <div class="col-sm-12">
                 <div id="slider-carousel" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
-                        <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#slider-carousel" data-slide-to="1"></li>
-                        <li data-target="#slider-carousel" data-slide-to="2"></li>
+                        @foreach ($slider as $row)
+                        <li data-target="#slider-carousel" data-slide-to="{{ $loop->index }}" class="{{ $loop->first ? 'active' : '' }}"></li>
+                        @endforeach
+                        
                     </ol>
                     
                     <div class="carousel-inner">
-                        <div class="item active">
+                        @foreach ( $slider as $row)
+                        <div class="item {{ $loop->first ? 'active' : '' }}">
                             <div class="col-sm-6">
                                 <h1><span>E</span>-SHOPPER</h1>
                                 <h2>Free E-Commerce Template</h2>
@@ -18,35 +23,10 @@
                                 <button type="button" class="btn btn-default get">Get it now</button>
                             </div>
                             <div class="col-sm-6">
-                                <img src="{{ asset ('frontend/images/home/girl1.jpg')}}" class="girl img-responsive" alt="" />
-                                <img src="{{ asset ('frontend/images/home/pricing.png')}}"  class="pricing" alt="" />
+                                <img src="{{ asset ($row->slider_image)}}" class="girl img-responsive" alt="" />
                             </div>
                         </div>
-                        <div class="item">
-                            <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
-                                <h2>100% Responsive Design</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <button type="button" class="btn btn-default get">Get it now</button>
-                            </div>
-                            <div class="col-sm-6">
-                                <img src="{{ asset ('frontend/images/home/girl2.jpg')}}" class="girl img-responsive" alt="" />
-                                <img src="{{ asset ('frontend/images/home/pricing.png')}}"  class="pricing" alt="" />
-                            </div>
-                        </div>
-                        
-                        <div class="item">
-                            <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
-                                <h2>Free Ecommerce Template</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <button type="button" class="btn btn-default get">Get it now</button>
-                            </div>
-                            <div class="col-sm-6">
-                                <img src="{{ asset('frontend/images/home/girl3.jpg')}}" class="girl img-responsive" alt="" />
-                                <img src="{{ asset('frontend/images/home/pricing.png')}}" class="pricing" alt="" />
-                            </div>
-                        </div>
+                        @endforeach
                         
                     </div>
                     
